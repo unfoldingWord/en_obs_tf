a29dfde6bbc6874e39d0932cd383a10763a1b8e2
ILindsley <ian.lindsley@unfoldingword.org>
2023-05-24 21:36:25 -0400

    continuing work on transformation script
    continuing work on transformation script

--- deployment_scripts/transform_md.py
+++ deployment_scripts/transform_md.py
@@ -1,16 +1,34 @@
 import sys
 import os
 
-if len(sys.argv) < 3:
-    print("Please provide two directory paths as command line arguments. The first should be the directory containing the markdown to transform. The second should be the output directory.")
-    sys.exit(1)
+def build_yaml_map(source_dir):
+    yaml_map = {}
+    if os.path.isdir(source_dir):
+        for filename in os.listdir(source_dir):
+            if filename.endswith(".md"):
+                file_path = os.path.join(source_dir, filename)
+                with open(file_path, "r") as file:
+                    yaml_dash_line_count = 0
+                    for line in file:
+                        if yaml_dash_line_count == 2:
+                            break
+                        if '---' in line:
+                            yaml_dash_line_count = yaml_dash_line_count + 1
+                        if filename not in yaml_map:
+                            yaml_map[filename] = []
+                        yaml_map[filename].append(line)
+    return yaml_map
 
-source_dir = sys.argv[1]
-output_dir = sys.argv[2]
 
-if os.path.isdir(source_dir):
-    for filename in os.listdir(source_dir):
-        if os.path.isfile(os.path.join(source_dir, filename)):
-            print(filename)
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        print("Please provide two directory paths as command line arguments. The first should be the directory containing the markdown to transform. The second should be the output directory.")
+        sys.exit(1)
+
+    source_dir = sys.argv[1]
+    output_dir = sys.argv[2]
+    build_yaml_map(source_dir)
+    print("successfully built yaml map.")
+
 else:
     print("Invalid directory path.")
