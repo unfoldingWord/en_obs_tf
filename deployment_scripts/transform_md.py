7c508fdbb33a927db56ba220cbd5cffc5128b23f
ILindsley <ian.lindsley@unfoldingword.org>
2023-05-24 22:04:44 -0400

    Updated workflow
    Updated workflow

--- deployment_scripts/transform_md.py
+++ deployment_scripts/transform_md.py
@@ -1,6 +1,7 @@
 import sys
 import os
 
+
 def build_yaml_map(source_dir):
     yaml_map = {}
     if os.path.isdir(source_dir):
@@ -15,9 +16,27 @@ def build_yaml_map(source_dir):
                         if '---' in line:
                             yaml_dash_line_count = yaml_dash_line_count + 1
                         if filename not in yaml_map:
+                            print(f'adding {filename} to yaml map...')
                             yaml_map[filename] = []
                         yaml_map[filename].append(line)
     return yaml_map
+
+
+def transform_markdown(source_dir, transform_dir):
+    yaml_map = build_yaml_map(source_dir)
+    if os.path.isdir(transform_dir):
+        for filename in os.listdir(transform_dir):
+            if filename.endswith(".md"):
+                file_path = os.path.join(transform_dir, filename)
+                if filename in yaml_map:
+                    print(f'fixing yaml for file {filename}...')
+                    lines = yaml_map[filename]
+                    with open(file_path, "r+") as file:
+                        content = file.read()
+                        file.seek(0, 0)  # Move the file pointer to the beginning
+                        for line in lines:
+                            file.write(line)  # Add each line to the file
+                        file.write(content)
 
 
 if __name__ == '__main__':
@@ -26,9 +45,9 @@ if __name__ == '__main__':
         sys.exit(1)
 
     source_dir = sys.argv[1]
-    output_dir = sys.argv[2]
-    build_yaml_map(source_dir)
-    print("successfully built yaml map.")
+    transform_dir = sys.argv[2]
+    transform_markdown(source_dir, transform_dir)
+    print("transformation complete.")
 
 else:
     print("Invalid directory path.")
