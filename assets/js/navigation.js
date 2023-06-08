8ab12b567ff3746fbfb6a60267686dfc350680f1
ILindsley <ian.lindsley@unfoldingword.org>
2023-06-07 20:33:49 -0400

    Fix for the header issue and possb. the image jiggle
    Fix for the header issue and possb. the image jiggle issue.

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -6,19 +6,19 @@ function addClass(nodeId, className) {
 }
 
 function selectActiveNavigationItem() {
-    console.log(window);
-    switch (window.location.pathname) {
-        case "/overview.html":
+    var filename = window.location.pathname.split("/").pop();
+    switch (filename) {
+        case "overview.html":
             addClass('introduction-header-nav-item', 'header-nav-item--active');
             addClass('introduction-header-nav-item-mobile', 'header-nav-item--active');
             addClass('introduction-header-nav-folder-item', 'header-menu-nav-item--active');
             break;
-        case "/mission-translation-brief.html":
+        case "mission-translation-brief.html":
             addClass('mtb-header-nav-item', 'header-nav-item--active');
             addClass('mtb-header-nav-item-mobile', 'header-nav-item--active');
             addClass('mtb-header-nav-folder-item', 'header-menu-nav-item--active');
             break;
-        case "/obs.html":
+        case "obs.html":
             addClass('obs-header-nav-item', 'header-nav-item--active');
             addClass('obs-header-nav-item-mobile', 'header-nav-item--active');
             addClass('obs-header-nav-folder-item', 'header-menu-nav-item--active');
