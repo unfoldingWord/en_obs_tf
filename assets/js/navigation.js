1d4619000cbb1730ef432b5c49c5725cd25ce044
ILindsley <ian.lindsley@unfoldingword.org>
2023-06-08 13:31:19 -0400

    Fix active header underline issue on mobile
    The active header was not being properly underlined on mobile. This change fixes the issue.

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -5,23 +5,30 @@ function addClass(nodeId, className) {
     }
 }
 
+function setAttribute(nodeId, attrName, attrValue) {
+    var node = document.getElementById(nodeId);
+    if (node != null) {
+        node.setAttribute(attrName, attrValue);
+    }
+}
+
 function selectActiveNavigationItem() {
     var filename = window.location.pathname.split("/").pop();
     switch (filename) {
         case "overview.html":
             addClass('introduction-header-nav-item', 'header-nav-item--active');
             addClass('introduction-header-nav-item-mobile', 'header-nav-item--active');
-            addClass('introduction-header-nav-folder-item', 'header-menu-nav-item--active');
+            setAttribute('introduction-header-nav-folder-item', 'aria-current', 'page');
             break;
         case "mission-translation-brief.html":
             addClass('mtb-header-nav-item', 'header-nav-item--active');
             addClass('mtb-header-nav-item-mobile', 'header-nav-item--active');
-            addClass('mtb-header-nav-folder-item', 'header-menu-nav-item--active');
+            setAttribute('mtb-header-nav-folder-item', 'aria-current', 'page');
             break;
         case "obs.html":
             addClass('obs-header-nav-item', 'header-nav-item--active');
             addClass('obs-header-nav-item-mobile', 'header-nav-item--active');
-            addClass('obs-header-nav-folder-item', 'header-menu-nav-item--active');
+            setAttribute('obs-header-nav-folder-item', 'aria-current', 'page');
             break;
     }
 }
